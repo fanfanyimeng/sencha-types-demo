d27b1985b3e122b93a87288f61592ab900a4bfe6
fanfanyimeng <zhengjh@microseer.com>
2020-03-25 18:32:06 +0800

    修改编译方式

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,49 @@
-module.exports = {
-    //唯一入口文件
-    entry: __dirname + "/src/index.ts",
-    output: {
-        //打包后的文件存放的地方
-        path: __dirname + "/public",
-        filename: "bundle.js"//打包后输出文件的文件名
-    },
-    devServer: {
-        contentBase: __dirname + "/public",
-        proxy:{
-            '/swagger.doc':{
-                target: "http://localhost:3000/",
-                changeOrigin: true
-            },
-            '/eureka': {
-                target: "http://localhost:3000/",
-                changeOrigin: true
-            }
-        }
-    },
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const publishPath = __dirname + "/build/publish/";
+const buildJsName = "bundle.js";
 
+module.exports = (env, argv) => {
+    console.info("测试一下");
+    console.info(JSON.stringify(argv));
+    const envMode = argv.mode;
+    var config = {
+        //唯一入口文件
+        entry: {
+            index: __dirname + "/src/index.ts"
+        },
+        output: {
+            //打包后的文件存放的地方
+            path: publishPath,
+            //打包后输出文件的文件名
+            filename: buildJsName//打包后输出文件的文件名
+        },
+        devServer: {
+            contentBase: __dirname + "/public",
 
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
+        },
+        devtool: envMode === "production" ? "cheap-module-source-map" : "source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js']
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: envMode === 'production' ? "JSON TOOL." : "JSON TOOL.(" + envMode + ")",
+                template: __dirname + '/public/index.html',
+                filename: publishPath + 'index.html',
+                chunks: ['index'],
+                inject: true,
+                hash: true
+            })
         ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    }
+    };
+    return config;
 };
